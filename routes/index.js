b5f8eccbb1392488757f31d52f390e2af09dc9ef
배지원 <bjiwon2005@naver.com>
2018-12-15 08:56:50 +0900

    add mypage class

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,12 @@ const user_router = require('./users/users-router');
 const class_router = require('./class/class-router');
 const home_router = require('./home/home-router');
 const question_router = require('./question/question-router');
+const mypage_router = require('./mypage/mypage-router');
 
 router.use('/users', user_router);
 router.use('/class', class_router);
 router.use('/home', home_router);
 router.use('/question', question_router);
+router.use('/mypage', mypage_router);
 
 module.exports = router;
---
+++ routes/mypage/mypage-router.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('../../module/jwt');
+const db = require('../../module/db');
+const check = require('../../module/check');
+
+router.get('/class', async (req, res) => {
+    let token = req.headers.token;
+    if (!token) {
+        res.status(400).json({
+            message: "No Token"
+        });
+    }
+    else {
+        let decoded = jwt.verify(token);
+
+        if (decoded === 10) {
+            res.status(500).send({
+                message: "token err",
+                expired: 1
+            });
+            return;
+        }
+        //토큰에 에러 있을 때
+        if (decoded === -1) {
+            res.status(500).send({
+                message: "token err" //여기서 400에러를 주면 클라의 문제니까 메세지만 적절하게 잘 바꿔주면 된다.
+            });
+        }
+        else {
+            let my_class = `select * from class where user_fk = ?`;
+            let apply_class = `select a.* from class a, my_class b where b.user_fk = ? and a.class_id = b.class_fk`;
+            let my_data = await db.queryParamArr(my_class, [decoded.user_idx]);
+            let apply_data = await db.queryParamArr(apply_class, [decoded.user_idx]);
+
+            if (!my_data || !apply_data) {
+                res.status(500).json({
+                    message: "Internal Server Error"
+                });
+            }
+            else {
+                res.status(200).json({
+                    my_class : my_data,
+                    apply_class : apply_data
+                });
+            }
+        }
+    }
+});
+
+module.exports = router;
