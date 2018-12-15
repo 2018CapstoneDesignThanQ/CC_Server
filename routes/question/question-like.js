7c81a3cfd0401b1c9133c8edd1d56d96ecf7b9e8
배지원 <bjiwon2005@naver.com>
2018-12-15 13:59:26 +0900

    modify

--- routes/question/question-like.js
+++ routes/question/question-like.js
@@ -68,7 +68,7 @@ router.post('/', async (req, res) => {
                             question_id : question_id
                         };
 
-                        if (add_like>3) {
+                        if (add_like.like_cnt>3) {
                             const alert_question = {
                                 question_id : question_id,
                                 title : question_data[0].title,
@@ -79,6 +79,95 @@ router.post('/', async (req, res) => {
                         io.of('/room').to(class_id).emit('addLike', add_like);
                         res.status(200).json({
                             message: "Success Add Like"
+                            // data: question_data
+                        });
+                    }
+                }).catch(error => {
+                    console.log(error);
+                    res.status(500).json({
+                        message: "Internal Server Error"
+                    })
+                });
+            }
+        }
+    }
+});
+
+router.patch('/', async (req, res) => {
+    let token = req.headers.token;
+    let class_id = req.body.class_id;
+    let question_id = req.body.question_id;
+
+    if (check.checkNull([token, class_id, question_id])) {
+        res.status(400).json({
+            message: "Null Value"
+        });
+    }
+    else {
+        let decoded = jwt.verify(token);
+
+        if (decoded === 10) {
+            res.status(500).send({
+                message: "token err",
+                expired: 1
+            });
+            return;
+        }
+        //토큰에 에러 있을 때
+        if (decoded === -1) {
+            res.status(500).send({
+                message: "token err" //여기서 400에러를 주면 클라의 문제니까 메세지만 적절하게 잘 바꿔주면 된다.
+            });
+        }
+        else {
+            let check_like = `select * from question_like where question_fk = ? and user_fk = ?`;
+            let check_result = await db.queryParamArr(check_like, [question_id, decoded.user_idx]);
+            if (!check_result) {
+                res.status(500).json({
+                    message: "Internal Server Error"
+                });
+            }
+            else if (check_result.length === 0) {
+                res.status(400).json({
+                    message: "None Already Like"
+                });
+            }
+            else {
+                console.log(check_result);
+                let transaction_result = await db.transactionControll(async (connection) => {
+                    let update_query = `UPDATE question SET like_cnt = like_cnt - 1 WHERE question_id = ?`;
+                    let delete_query = `delete from question_like where question_fk = ? and user_fk = ?`;
+                    await connection.query(update_query, [question_id]);
+                    await connection.query(delete_query, [question_id, decoded.user_idx]);
+
+                    let select_question = `select (select count( * ) as like_cnt from question_like as a_like where a_like.question_fk = b.question_id  and a_like.user_fk = ? ) as is_like, a.nickname, b.* from users a, question b where b.question_id = ? and a.user_id = b.user_fk`;
+                    let question_data = await connection.query(select_question, [decoded.user_idx, question_id]);
+                    let select_top3 = `select a.*, b.nickname from question a, users b order by a.like_cnt limit 3`;
+                    let top3_data = await connection.query(select_top3);
+                    if (!question_data || !top3_data) {
+                        res.status(500).json({
+                            message: "Internal Server Error"
+                        });
+                    }
+                    else {
+                        const io = req.app.get('io');
+                        io.of('/room').to(class_id).emit('top3', top3_data);
+                        let cancel_like = {
+                            like_cnt : question_data[0].like_cnt,
+                            question_id : question_id
+                        };
+
+                        if (cancel_like.like_cnt>3) {
+                            const alert_question = {
+                                question_id : question_id,
+                                title : question_data[0].title,
+                                content : question_data[0].content
+                            }
+                            io.of('/room').to(class_id).emit('alertWeb', alert_question);
+                        }
+                        io.of('/room').to(class_id).emit('addLike', cancel_like);
+                        res.status(200).json({
+                            message: "Success Cancel Like"
                             // data: question_data
                         });
                     }
